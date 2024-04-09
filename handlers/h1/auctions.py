452acdd9c7b14f1affe15b9e0a870b85d83b5810
Is Teamoor <is.teamoor@gmail.com>
2024-04-09 09:17:31 +0500

    bids table

--- handlers/h1/auctions.py
+++ handlers/h1/auctions.py
@@ -1,22 +1,49 @@
 from aiogram import types
 from aiogram.types.web_app_info import WebAppInfo
+import json
 
 import time
 
 from . import calc
 
 webApp_URL = 'https://isteamoor.github.io/webApp.github.io/'
+click_test = '398062629:TEST:999999999_F91D8F69C042267444B74CC0B3C747757EB0E065'
+payme_test = '371317599:TEST:1712560294201'
+
+
 
 
 async def appButton(message):
     timestamp = str( int(time.time()) )
-    url = webApp_URL + "?timestamp=" + timestamp
+    uniqUrl = webApp_URL + "?timestamp=" + timestamp
 
-    inline_btn = types.InlineKeyboardButton(text='Открыть сайт внутри телеграм', web_app=WebAppInfo(url=url))
-    inline_kb = types.InlineKeyboardMarkup(inline_keyboard=[[inline_btn]])
-    
-    await message.reply("Веб приложение", reply_markup=inline_kb)
+    buttons = [[types.InlineKeyboardButton(text="Перейти в Mini App", web_app=WebAppInfo(url=uniqUrl) ),],]
+    keyboard = types.InlineKeyboardMarkup(inline_keyboard=buttons)
 
-
+    await message.reply("Веб приложение", reply_markup=keyboard)
 
 
+
+
+async def sendInvoice(message, bot):
+    await bot.send_invoice(message.chat.id, 
+                           title = 'Тестирование оплаты',
+                           description = 'Тестирование оплаты поле description',
+                           payload = json.dumps({'user_id':message.chat.id}),
+                           currency = 'UZS', 
+                           prices=[types.LabeledPrice(label='Тестовая покупка', amount=10000000)],
+                           provider_token = click_test) 
+
+
+
+async def preCheckPay(pre_checkout_query, bot):
+    await bot.answer_pre_checkout_query(pre_checkout_query.id, ok=True)
+
+async def successPay(message, bot):
+    payment_info = message.successful_payment.to_python()
+    user_id = payment_info['user_id']
+    amount = payment_info['total_amount']
+    currency = payment_info['currency']
+    invoice_payload = payment_info['invoice_payload']
+
+    await bot.send_message(user_id, f"Спасибо за оплату в размере {amount} {currency}. Payload = {invoice_payload}!")
--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import asyncio
 from aiogram import Bot, Dispatcher, F
-from aiogram.types import Message
+from aiogram.types import Message, PreCheckoutQuery
 from aiogram.filters.command import Command
+from aiogram.enums.content_type import ContentType
+
 
 from handlers.gen import general
 from handlers.h1 import auctions
@@ -18,10 +20,25 @@ async def start(message: Message):
     await general.start(message)
 
 
+
+
 ### [Auctions]
 @dp.message(F.text.lower() == "1")
 async def link_to_webApp(message: Message):
     await auctions.appButton(message)
+
+@dp.message(F.text.lower() == "2")
+async def userPayment(message: Message):
+   await auctions.sendInvoice(message, bot)
+
+@dp.pre_checkout_query()
+async def preCheckPayment(pre_checkout_query: PreCheckoutQuery):
+    await auctions.preCheckPay(pre_checkout_query, bot)
+
+@dp.message(F.successful_payment != None)
+async def successfulPayment(message:Message):
+    await auctions.successPay(message, bot)
+
 
 
 
